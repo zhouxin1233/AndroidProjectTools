186514bb7ceb11248781f1e613bdac38b5cabf3a
zhouxin1233 <2215012841@qq.com>
2016-12-18 10:42:57 +0800

    第一次建立News项目

---
+++ news/src/main/java/com/news/MainActivity.java
@@ -0,0 +1,32 @@
+package com.news;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
+public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.tv)TextView mTv;
+    @BindView(R.id.btn)Button mBtn;
+    @BindView(R.id.activity_main)LinearLayout mActivityMain;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
+    }
+
+    @OnClick(R.id.btn)
+    public void onClick() {
+        Toast.makeText(this, "被点击啦", Toast.LENGTH_SHORT).show();
+    }
+}
