05dd89a97df3c009643c93b43fb42c80d395b691
zhouxin1233 <2215012841@qq.com>
2017-01-02 18:04:48 +0800

    对fragment 懒加载的笔记

--- app/src/main/java/com/tools/base/BaseLazyFragment.java
+++ app/src/main/java/com/tools/base/BaseLazyFragment.java
@@ -8,6 +8,11 @@ import android.support.v4.app.Fragment;
 
 public abstract class BaseLazyFragment extends Fragment {
     protected boolean isVisible;
+    //setUserVisibleHint()视图是否已经对用户可见，系统的方法 会在attach方法之前执行
+    // 在上个页面预加载进入后,再进入的时候就只会走setUserVisibleHint 方法 ,并返回true
+    // 所以需要用个boolean值判断 是第几次调用setUserVisibleHint 预加载的时候调用一次,真正可见的时候还会调用一次
+    //通常做法 在setUserVisibleHint 和 onCreateView 均要调用加载数据的方法
+    // ==>只不过这个加载的数据方法得先判断有没有初始化过(确保走完onCreateView等预加载方法),然后再根据getUserVisibleHint判断是不是可见状态
 //    @Override
 //    public void setUserVisibleHint(boolean isVisibleToUser) {
 //        super.setUserVisibleHint(isVisibleToUser);
